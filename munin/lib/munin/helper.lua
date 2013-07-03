5a7b873b4fe94abe52e3651fe3ade728e88cd007
Ard van Breemen <ard@kwaak.net>
2013-07-03 13:17:44 +0200

    modularize functions, add test env

---
+++ munin/lib/munin/helper.lua
@@ -0,0 +1,33 @@
+local m={}
+--- Generic functions
+-- printf, short for..
+function m.printf(fmt,...)
+        io.write(string.format(fmt,...))
+end
+-- split a line into fields
+function m.split(f,l)
+        local v={}
+        local fn=1
+        for d in string.gmatch(l," (%S+)") do
+                v[f[fn]]=d
+                fn = fn+1
+        end
+        return v
+end
+-- fillout replacement
+function m.fillout(t,s)
+        return (t:gsub("${([%a]+)}", function(m) return s[m] or m end))
+end
+
+function m.fwrite(t,s)
+        io.write(fillout(t,s))
+end
+--- Specific
+-- Normalice ifname and strip :
+function m.normalizeif(s)
+        s=string.gsub(s,"[#.]","_")
+        s=string.gsub(s,":","")
+        return s
+end
+return m
+
